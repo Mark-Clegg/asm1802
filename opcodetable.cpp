9d496634433bc8a5e9047d805573e38d344a62cf
Mark Clegg <mark.clegg2@gmail.com>
2023-11-16 11:08:06 +0000

    Remove WORD pseudo-op

--- opcodetable.cpp
+++ opcodetable.cpp
@@ -137,7 +137,6 @@ const std::map<std::string, OpCodeSpec> OpCodeTable::OpCode =
     { "ORG",        { ORG,       PSEUDO_OP,             CPU_1802  }},
     { "DB",         { DB,        PSEUDO_OP,             CPU_1802  }},
     { "DW",         { DW,        PSEUDO_OP,             CPU_1802  }},
-    { "WORD",       { DW,        PSEUDO_OP,             CPU_1802  }},
     { "ALIGN",      { ALIGN,     PSEUDO_OP,             CPU_1802  }},
     { "ASSERT",     { ASSERT,    PSEUDO_OP,             CPU_1802  }},
     { "MACRO",      { MACRO,     PSEUDO_OP,             CPU_1802  }},
